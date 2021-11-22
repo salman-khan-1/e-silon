1d1c4e97e8d8d0005c587332dbb5199750802a30
salman-khan-Atk <75664644+salman-khan-1@users.noreply.github.com>
2021-11-22 23:13:55 +0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Site Metas -->
+    <title>karwan</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/logo-icon.png" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/logo-icon.png">
+    
+    <!-- Design fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet"> 
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- FontAwesome Icons core CSS -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive styles for this template -->
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!-- Colors for this template -->
+    <link href="css/colors.css" rel="stylesheet">
+
+    <!-- Version Tech CSS for this template -->
+    <link href="css/version/tech.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+    <div id="wrapper">
+    <header class="header">
+            <a href="index.php" class="header__logo img-fluid"><SPAN STYLE="COLOR:red; FONT-SIZE:27PX;">K</SPAN><SPAN STYLE=" FONT-SIZE:20PX;">ARWAN</SPAN></a>
+   
+            <ion-icon name="menu-outline" class="header__toggle" id="nav-toggle"></ion-icon>
+
+            <nav class="nav" id="nav-menu">
+                
+                <div class="nav__content bd-grid">
+                
+                    <ion-icon name="close-outline" class="nav__close" id="nav-close"></ion-icon>
+    
+                    
+    
+                    <div class="nav__menu">
+                        <ul class="nav__list">
+                            <li class="nav__item"><a href="" class="nav__link ">Home</a></li>
+                            <li class="nav__item"><a href="courses.php" class="nav__link ">Learn</a></li>
+                            <li class="nav__item"><a href="" class="nav__link ">projects</a></li>
+                            <li class="nav__item"><a href="" class="nav__link ">blogs</a></li>
+                            <li class="nav__item"><a href="#" class="nav__link ">About</a></li>
+                            <li class="nav__item"><a href="contact-us.php" class="nav__link ">Contact</a></li>
+                        </ul>
+                    </div>
+                    
+                   
+                   
+                </div>
+    
+               
+
+            </nav>
+            <div class="search-container">
+                    <form action="search.php" method="get">
+                                          <input class="search expandright" id="searchright" type="search" name="search" placeholder="Search">
+                     <label class="button searchbutton"  for="searchright"><span class="mglass"  >&#9906;</span></label>
+                    </form>
+                </div>
+        </header>
+         <!-- ===== IONICONS ===== -->
+         <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+
+<!--===== MAIN JS =====-->
+<script src="js/main.js"></script>
