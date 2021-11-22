e3d3046bf7f7260e931a8f5cd4786ce760dc9abe
salman-khan-Atk <75664644+salman-khan-1@users.noreply.github.com>
2021-11-22 23:38:51 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,81 +1,82 @@
 <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <div class="widget">
-                            <div class="footer-text text-left">
-                                <a href="index.html"><img src="upload/logo-text.png" alt="" class="img-fluid"></a>
-                                <p> we sharing marketing, news and gadget articles.</p>
-                                <div class="social">
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>              
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa fa-instagram"></i></a>
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google Plus"><i class="fa fa-google-plus"></i></a>
-                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
-                                </div>
+    <div class="container foo-top">
+      <div class="row">
+        <div class="col-md-2 col-sm-6">
+          <div class="widget widget-navigation">
+            <h4 class="widget-title">About us</h4>
+            <ul>
+              <li><a href="#">Who we are</a></li>
+              <li><a href="#">Work with us</a></li>
+              <li><a href="#">Become a supplier</a></li>
+              <li><a href="#">Investor relations</a></li>
+              <li><a href="#">Our devices</a></li>
+              <li><a href="#">Affiliate program</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-2 offset-md-1 col-sm-6">
+          <div class="widget widget-navigation">
+            <h4 class="widget-title">Useful links</h4>
+            <ul>
+              <li><a href="#">Who we are</a></li>
+              <li><a href="#">Work with us</a></li>
+              <li><a href="#">Become a supplier</a></li>
+              <li><a href="#">Investor relations</a></li>
+              <li><a href="#">Our devices</a></li>
+              <li><a href="#">Affiliate program</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-2 offset-md-1 col-sm-6">
+          <div class="widget widget-navigation">
+            <h4 class="widget-title">Online shopping</h4>
+            <ul>
+              <li><a href="#">Who we are</a></li>
+              <li><a href="#">Work with us</a></li>
+              <li><a href="#">Become a supplier</a></li>
+              <li><a href="#">Investor relations</a></li>
+              <li><a href="#">Our devices</a></li>
+              <li><a href="#">Affiliate program</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 offset-md-1 col-sm-6">
+          <div class="widget widget-app">
+            <h4 class="widget-title">experience app on mobile</h4>
+            <div class="app-btn">
+              <a href="#" class="google-play">
+                <i class="fas fa-robot"></i>
+                <p><span>get it on</span>Goole play</p>
+              </a>
+              <a href="#" class="app-store">
+                <i class="fab fa-apple"></i>
+                <p><span>get it on</span>app store</p>
+              </a>
+            </div>
+          </div>
+          <div class="widget widget-social">
+            <h4 class="widget-title">Online shopping</h4>
+            <div class="social-media">
+              <a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-pinterest-p"></i></a><a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-                                <hr class="invis">
-
-                                <div class="newsletter-widget text-left">
-                                    <form class="form-inline">
-                                        <input type="text" class="form-control" placeholder="Enter your email address">
-                                        <button type="submit" class="btn btn-primary">SUBMIT</button>
-                                    </form>
-                                </div><!-- end newsletter -->
-                            </div><!-- end footer-text -->
-                        </div><!-- end widget -->
-                    </div><!-- end col -->
-
-                    <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
-                        <div class="widget">
-                            <h2 class="widget-title">Popular Categories</h2>
-                            <div class="link-widget">
-                                <ul>
-                                    <li><a href="#">Marketing <span>(21)</span></a></li>
-                                    <li><a href="#">SEO Service <span>(15)</span></a></li>
-                                    <li><a href="#">Digital Agency <span>(31)</span></a></li>
-                                    <li><a href="#">Make Money <span>(22)</span></a></li>
-                                    <li><a href="#">Blogging <span>(66)</span></a></li>
-                                </ul>
-                            </div><!-- end link-widget -->
-                        </div><!-- end widget -->
-                    </div><!-- end col -->
-
-                    <div class="col-lg-2 col-md-12 col-sm-12 col-xs-12">
-                        <div class="widget">
-                            <h2 class="widget-title">Copyrights</h2>
-                            <div class="link-widget">
-                                <ul>
-                                    <li><a href="#">About us</a></li>
-                                    <li><a href="#">Advertising</a></li>
-                                    <li><a href="#">Write for us</a></li>
-                                    <li><a href="#">Trademark</a></li>
-                                    <li><a href="#">License & Help</a></li>
-                                </ul>
-                            </div><!-- end link-widget -->
-                        </div><!-- end widget -->
-                    </div><!-- end col -->
-                </div>
-
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <br>
-                        <div class="copyright">&copy; Karwan</a>.</div>
-                    </div>
-                </div>
-            </div><!-- end container -->
-        </footer><!-- end footer -->
-
-        <div class="dmtop">Scroll to Top</div>
-        
-    </div><!-- end wrapper -->
-
-    <!-- Core JavaScript
-    ================================================== -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/tether.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
-
+    <div class="container">
+      <p class="copyright">Copyright © 2019 <a href="#">domain.com</a>. All rights reserved.</p>
+    </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
